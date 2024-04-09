0b860de7454f717baddb6312caf2ccd25b81e0b4
Lucas Brito <luzabri7@gmail.com>
2024-04-09 15:08:30 -0400

    more collect bugs

--- altera/collect_raw_data.py
+++ altera/collect_raw_data.py
@@ -6,10 +6,11 @@ fname = 'C:\\Users\\student\\Desktop\\quantum-optics\\raw_altera_data.txt'
 bstring = bytearray()
 max_iter = 10
 i = 0 
-ser = serial.Serial('COM8', 19200, timeout=0, rtscts=1)
+ser = serial.Serial('COM8', 19200, timeout=0,)
 while i < max_iter: 
 	bstring += ser.read(512)
 	time.sleep(1)
+	i += 1
 
 
 f = open(fname, 'w')
